d230d3fafa3aca58785166e776b6b70c0c5eca21
JuhainaMT <juhaina@mvp-apps.ae>
2026-03-07 23:00:15 +0530

    feat: changed link peview

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
     siteName: "SZ Developers",
     images: [
       {
-        url: "/sz favicon.svg",
+        url: "/sz favicon.png",
         width: 1200,
         height: 630,
         alt: "SZ Developers - Real Estate",
@@ -29,7 +29,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "SZ Developers - Real Estate",
     description: "SZ Developers - Premium Real Estate Projects in Wayanad and Calicut",
-    images: ["/sz favicon.svg"],
+    images: ["/sz favicon.png"],
   },
   metadataBase: new URL("https://www.szdevelopers.com/"),
 };
@@ -51,13 +51,13 @@ export default function RootLayout({
         <meta property="og:url" content="https://www.szdevelopers.com/" />
         <meta property="og:title" content="SZ Developers - Real Estate" />
         <meta property="og:description" content="SZ Developers - Premium Real Estate Projects in Wayanad and Calicut" />
-        <meta property="og:image" content="https://www.szdevelopers.com/sz favicon.svg" />
+        <meta property="og:image" content="https://www.szdevelopers.com/sz favicon.png" />
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.szdevelopers.com/" />
         <meta property="twitter:title" content="SZ Developers - Real Estate" />
         <meta property="twitter:description" content="SZ Developers - Premium Real Estate Projects in Wayanad and Calicut" />
-        <meta property="twitter:image" content="https://www.szdevelopers.com/sz favicon.svg" />
+        <meta property="twitter:image" content="https://www.szdevelopers.com/sz favicon.png" />
       </head>
       <body>{children}</body>
     </html>
