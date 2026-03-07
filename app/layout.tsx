1fb105ba1aa0f779507713a7fc0ead77ebba4c9e
JuhainaMT <juhaina@mvp-apps.ae>
2026-03-07 22:43:29 +0530

    feat: link preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./styles.css";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "SZ Developers - Real Estate",
   description: "SZ Developers - Premium Real Estate Projects in Wayanad and Calicut",
   icons: {
@@ -47,9 +36,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
@@ -70,11 +59,7 @@ export default function RootLayout({
         <meta property="twitter:description" content="SZ Developers - Premium Real Estate Projects in Wayanad and Calicut" />
         <meta property="twitter:image" content="https://www.szdevelopers.com/sz favicon.svg" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
